05ee557d3b6d07de1f59d975102b5aa632aee37b
Egon Firman <egon.firman@tokopedia.com>
2021-12-31 11:16:14 +0700

    fix warnings in DiffUtil.kt

--- app/src/main/java/com/egon12/developerhelper/DiffUtil.kt
+++ app/src/main/java/com/egon12/developerhelper/DiffUtil.kt
@@ -3,9 +3,10 @@ package com.egon12.developerhelper
 import androidx.recyclerview.widget.DiffUtil
 import kotlin.reflect.KClass
 
-class UuidDiffUtil<T: HasUUID>(o: KClass<T>): DiffUtil.ItemCallback<T>() {
+class UuidDiffUtil<T : HasUUID>(@Suppress("UNUSED_PARAMETER") o: KClass<T>) :
+    DiffUtil.ItemCallback<T>() {
 
     override fun areItemsTheSame(oldItem: T, newItem: T): Boolean = oldItem.uuid == newItem.uuid
 
-    override fun areContentsTheSame(oldItem: T, newItem: T): Boolean = oldItem.equals(newItem)
+    override fun areContentsTheSame(oldItem: T, newItem: T): Boolean = oldItem == newItem
 }
